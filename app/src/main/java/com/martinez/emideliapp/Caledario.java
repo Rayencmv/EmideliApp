c9fb0342fbb634474ef5e018fe2d6b247ef41ce3
RayenM <r.martinez.valenzuela00@gmail.com>
2022-05-01 15:56:00 -0400

    Menu Listo

---
+++ app/src/main/java/com/martinez/emideliapp/Caledario.java
@@ -0,0 +1,16 @@
+package com.martinez.emideliapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Caledario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_caledario);
+    }
+
+
+}
---
+++ app/src/main/java/com/martinez/emideliapp/Menu.java
@@ -0,0 +1,37 @@
+package com.martinez.emideliapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+
+    public void CambioCalendario(View view){
+        Intent AbrirCalendario = new Intent(Menu.this ,Caledario.class);
+        startActivity(AbrirCalendario);
+    }
+
+    public void CambioPedido(View view){
+        Intent AbrirPedido = new Intent(Menu.this ,Pedido.class);
+        startActivity(AbrirPedido);
+    }
+
+    public void CambioRegistroU(View view){
+        Intent AbrirUsuario = new Intent(Menu.this ,Usuario.class);
+        startActivity(AbrirUsuario);
+    }
+
+    public void CambioRegistroC(View view){
+        Intent AbrirCliente = new Intent(Menu.this ,Cliente.class);
+        startActivity(AbrirCliente);
+    }
+
+}
